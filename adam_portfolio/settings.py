6be114e478a2b17d388fc8a835808ee03464a330
aboczek <adam.boczekwork@gmail.com>
2023-04-23 16:36:59 +0000

    test again

--- adam_portfolio/settings.py
+++ adam_portfolio/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["pp4-deployment.herokuapp.com/", "localhost"]
+ALLOWED_HOSTS = ["pp4-deployment.herokuapp.com", "localhost"]
 
 # Application definition
 
